321cb84c8a1ab2650e0c56d149e11ba5301a51fc
Milo MacPhail <40954168+milomacphail@users.noreply.github.com>
2019-07-24 21:10:40 -0400

    Connected backend to frontend

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharp_Scraper.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharp_Scraper.Startup))]
+namespace CSharp_Scraper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
