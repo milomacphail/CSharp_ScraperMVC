54b8b354005a1a968cbdc46f1a979c93d899f4c9
milomacphail <milomacphail@gmail.com>
2019-10-20 14:21:40 -0400

    file reorg

--- Scrape.cs
+++ Scrape.cs
@@ -2,9 +2,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_Scraper
 {
